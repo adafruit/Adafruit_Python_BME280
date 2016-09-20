6c29413ed38222fcafde7648dfe79570b2e2fd8c
Ryan <ryankurte@users.noreply.github.com>
2016-09-20 15:13:15 +1200

    Update setup.py
    Fix typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='Adafruit_BME280',
       version='1.0',
-      description='Adafruit Python MBE280 Liibrary',
+      description='Adafruit Python BME280 Liibrary',
       url='https://github.com/adafruit/Adafruit_Python_BME280',
       py_modules=['Adafruit_BME280'],
      )
