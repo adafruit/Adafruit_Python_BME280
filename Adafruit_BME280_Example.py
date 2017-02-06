0c5024197fc736edae7f7342f852a99bff31182a
Donna Whisnant <dewhisna@users.noreply.github.com>
2017-02-06 09:04:38 -0600

    Removed printing of 'Timestamp' in the example program, as 'sensor.t_fine' isn't a 'timestamp', but a raw temperature compensation value.

--- Adafruit_BME280_Example.py
+++ Adafruit_BME280_Example.py
@@ -7,7 +7,6 @@ pascals = sensor.read_pressure()
 hectopascals = pascals / 100
 humidity = sensor.read_humidity()
 
-print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
 print 'Temp      = {0:0.3f} deg C'.format(degrees)
 print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
 print 'Humidity  = {0:0.2f} %'.format(humidity)
